8ae5ff517fbc02b28971ad56cc257762c0f3ea09
Nikhil Bishnoi <bishnoinikhil35@gmail.com>
2026-08-05 16:03:41 +0530

    work on ui

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     const path=usePathname()
     return (
         <>
-            <div className='flex justify-between m-4 '>
+            <div className='flex justify-between  border-b-2 border-zinc-300 m-2'>
                 <div className='flex items-center'>
                     <Image src="/navicon.png" alt='icon' width={40} height={40} priority className='w-auto h-auto '></Image>
                     <h1 className='text-xl '>
@@ -19,9 +19,9 @@ const Navbar = () => {
                 <li className={path==="/explore"?'pb-1 border-b-2 border-blue-600 text-blue-800 transition-all duration-300 ':''}><Link href="/explore">Explore</Link></li>
                 <li className={path==="/create"?'pb-1 border-b-2 border-blue-600 text-blue-800 transition-all duration-300 ':''}><Link href="/create">Create</Link></li>
                </ul>
-               <div className='flex gap-4'>
-                <Link href="/">Login</Link>
-                <Link href="/">SignIn</Link>
+               <div className='flex gap-4 items-center'>
+                <Link href="/"className='bg-zinc-200 p-2  rounded-md text-black'>Login</Link>
+                <Link href="/" className='bg-blue-600 p-2  rounded-md text-white'>SignIn</Link>
                </div>
             </div>
         </>
